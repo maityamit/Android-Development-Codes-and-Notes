aada00f393efe67a39f768706b91706558f93ca4
AMIT <maityamit9800@gmail.com>
2021-10-21 15:26:36 +0530

    some changes

---
+++ Code with Harry Practice/01 - Chapter/GoodMorningShow/app/src/main/java/goodmorning/example/goodmorningshow/MainActivity.java
@@ -0,0 +1,28 @@
+package goodmorning.example.goodmorningshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Good Morning !", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
